47c0cff93b88214344c5c2c490f489ad79de5d4f
Nick Lehmann <nickmann97@gmail.com>
2017-12-04 21:20:20 +0100

    Dynamically build timetable layout

--- app/src/main/java/com/example/s0712338/myapplication/MainActivity.java
+++ app/src/main/java/com/example/s0712338/myapplication/MainActivity.java
@@ -3,22 +3,65 @@ package com.example.s0712338.myapplication;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    public TableLayout tableLayout;
+
+    public String[] lessonTimes = new String[10];
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TableRow tr2 = (TableRow) findViewById(R.id.tr2);
-        tr2.setBackgroundColor(Color.LTGRAY);
-        TableRow tr4 = (TableRow) findViewById(R.id.tr4);
-        tr4.setBackgroundColor(Color.LTGRAY);
+        tableLayout = (TableLayout) findViewById(R.id.tableLayout);
+
+        initLessonTimes();
+
+        buildTimetableLayout();
     }
-    public void changeClass(View view) {
-        ;
+
+    public void initLessonTimes() {
+        lessonTimes[0] = "7:30";
+        lessonTimes[1] = "9:20";
+        lessonTimes[2] = "11:10";
+        lessonTimes[3] = "13:00";
+        lessonTimes[4] = "14:50";
+    }
+
+    public void buildTimetableLayout() {
+        for ( int i = 1; i <= 5; i++ ) {
+            TableRow newRow = new TableRow(this);
+
+            TextView newTextView = new TextView(this);
+            newTextView.setText(lessonTimes[i - 1]);
+            newRow.addView(newTextView);
+
+            for (int j = 1; j <= 5; j++) {
+                EditText newEditText = new EditText(this);
+                newRow.addView(newEditText);
+
+                newEditText.setLayoutParams(new TableRow.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, 1));
+                newEditText.setGravity(Gravity.CENTER_HORIZONTAL);
+            }
+
+            tableLayout.addView(newRow);
+
+            TableLayout.LayoutParams params = (TableLayout.LayoutParams) newRow.getLayoutParams();
+            params.leftMargin = 30;
+            params.rightMargin = 30;
+            newRow.setLayoutParams(params);
+
+            if ( i % 2 == 0 ) {
+                newRow.setBackgroundColor(Color.LTGRAY);
+            }
+        }
     }
 }
